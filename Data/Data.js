c0209eae19d0b97afff1ff7e654ac695fbe19e30
AlongNgu <thanhbesthresh@gmail.com>
2022-07-09 00:10:09 +0700

    first commit

--- Data/Data.js
+++ Data/Data.js
@@ -31,37 +31,4 @@ const WhiteListSchema = new mongoose.Schema({
 	}
 })
 
-const mongoose = require('mongoose');
-
-const WhiteListSchema = new mongoose.Schema({
-	IP: {
-		type: String,
-		default: ""
-	},
-	Checkpoint1: {
-		type: Boolean,
-		default: false
-	},
-	Checkpoint2: {
-		type: Boolean,
-		default: false
-	},
-	UrlSolved: {
-		type: String,
-		default: ""
-	},
-	IsGenerate: {
-		type: Boolean,
-		default: false
-	},
-	KeyCode: {
-		type: String,
-		default: ""
-	},
-	TimeGenerate: {
-		type: Date,
-		default: 0
-	}
-})
-
 module.exports = mongoose.model("DataUser", WhiteListSchema)
